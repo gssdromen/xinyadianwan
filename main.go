368afc841764484dd165c5c8bac658d371effaee
gssdromen <gssdromen@126.com>
2017-09-05 10:02:43 +0800

    修改关注的游戏

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// var HomePageUrl = "http://www.xinyadianwan.com/bbs/index.php"
 	// var LoginPageUrl = "http://www.xinyadianwan.com/bbs/logging.php?action=login"
 	var PS4PageURL = "http://www.xinyadianwan.com/bbs/exchangeps4/index.php?page_c=1&search_name=&gametype=&gamelang=&page="
-	var targetGameNames = []string{"机战V", "深夜", "尼尔机械部队中文版", "进击的巨人", "奥丁领域中文版", "最终幻想15中文版", "心理测量者", "高达VS", "女神异闻录", "SD高达G世纪"}
+	var targetGameNames = []string{"机战V", "深夜", "尼尔机械部队中文版", "进击的巨人", "最终幻想15中文版", "心理测量者", "高达VS", "女神异闻录5中", "SD高达G世纪"}
 
 	var notify *notificator.Notificator = notificator.New(notificator.Options{
 		DefaultIcon: "icon/default.png",
