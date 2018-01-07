38e3d995541f07cd3bf860abd0bf8d0026ba6537
Cedric Wu <cedric.wu@chinapex.com>
2018-01-07 21:47:30 +0800

    更换关注的游戏

--- main.go
+++ main.go
@@ -18,7 +18,9 @@ func main() {
 	// var HomePageUrl = "http://www.xinyadianwan.com/bbs/index.php"
 	// var LoginPageUrl = "http://www.xinyadianwan.com/bbs/logging.php?action=login"
 	var PS4PageURL = "http://www.xinyadianwan.com/bbs/exchangeps4/index.php?page_c=1&search_name=&gametype=&gamelang=&page="
-	var targetGameNames = []string{"机战V", "深夜", "尼尔机械部队中文版", "进击的巨人", "最终幻想15中文版", "心理测量者", "高达VS", "女神异闻录5中", "SD高达G世纪"}
+
+	var targetGameNames []string
+	targetGameNames = []string{"深夜", "尼尔机械部队中文版", "进击的巨人", "最终幻想15中文版", "心理测量者", "高达VS", "女神异闻录5中", "SD高达G世纪", "丧尸围城", "怪物猎人", "洛克人遗产"}
 
 	var notify *notificator.Notificator = notificator.New(notificator.Options{
 		DefaultIcon: "icon/default.png",
@@ -99,6 +101,14 @@ func getDocument(url string) *goquery.Document {
 		fmt.Println(err.Error())
 		panic(err)
 	}
-
 	return doc
 }
+
+// func getTatgetGamesFormFile(filename string) (list []string) {
+// 	file, _ := os.Open(filename)
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		line := scanner.Text()
+// 		line := strings.TrimSpace(line)
+// 	}
+// }
